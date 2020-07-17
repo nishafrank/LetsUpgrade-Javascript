ec68ad079d9d92082a13d219d2fbede7156c1cc1
nishafrank <68235129+nishafrank@users.noreply.github.com>
2020-07-17 23:14:30 +0400

    Add files via upload
    Day 3 Assignment

---
+++ Day 3 JavaScript Assignment/scripts/evenodd.js
@@ -0,0 +1,8 @@
+var numstr= window.prompt("Enter the number ");
+var numint = parseInt(numstr);
+    if(numint % 2==0)
+    console.log(numint + " is Even");
+    else
+    console.log(numint + " is Odd");
+
+
